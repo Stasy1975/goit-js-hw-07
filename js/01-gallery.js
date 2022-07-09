5bcab9d745da3296d0c288be9789070e1869dbf8
Anastasiia Nedbai <nedbayfomina@gmail.com>
2022-07-09 11:49:10 +0300

    error

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,42 @@
+// Создание и рендер разметки по массиву данных galleryItems и предоставленному шаблону элемента галереи.
+// Реализация делегирования на div.gallery и получение url большого изображения.
+// Подключение скрипта и стилей библиотеки модального окна basicLightbox. Используй CDN сервис jsdelivr и добавь в проект ссылки на минифицированные (.min) файлы библиотеки.
+// Открытие модального окна по клику на элементе галереи. Для этого ознакомься с документацией и примерами.
+// Замена значения атрибута src элемента <img> в модальном окне перед открытием. Используй готовую разметку модального окна с изображением из примеров библиотеки basicLightbox.
+
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryEl = document.querySelector('.gallery');
+
+function galleryGreat(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `<div class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"/>
+   </a>
+</div>`;
+    })
+    .join(' ');
+}
+// const galleryUl = document.createElement('ul');
+// galleryUl.classList.add('gallery__list');
+galleryEl.insertAdjacentHTML('afterbegin', galleryGreat(galleryItems));
+// galleryEl.appendChild(galleryUl);
+
+galleryEl.addEventListener('click', onClickPicture);
+
+function onClickPicture(e) {
+  e.preventDefault();
+  if (!e.target.classList.contains('gallery__image')) {
+    return;
+  }
+  const instance = basicLightbox.create(`<img src="${e.target.dataset.sourсe}">`);
+
+  instance.show();
+}
